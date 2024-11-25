15d7426a93bddcedbb385d423b8a2074cb321f3c
qwopper <forsaken0915@gmail.com>
2024-11-24 18:22:57 -0700

    Made changes to numpy-basics/numpy-array-operations.ipynb and numpy-basics/numpy-matrix-operations.py

--- numpy-basics/numpy-matrix-operations.py
+++ numpy-basics/numpy-matrix-operations.py
@@ -1,1 +1,11 @@
+import numpy as np
 
+# Creating arrays
+array_1d = np.array([1, 2, 3, 4, 5]) # 1D array
+array_2d = np.array([[1, 2, 3], [4, 5, 6]]) # 2D array
+
+# Array Properties
+print("1D Array:", array_1d)
+print("Shape:", array_1d.shape)
+print("2D Array:", array_2d)
+print("Shape:", array_2d.shape)
